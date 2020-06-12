6e02c834ad76ce00ffbde4eccc9bce4505a76da8
Ivan Cheng <ivan@ivancheng.co.uk>
2020-06-12 18:26:51 +0100

    css update

--- website/app.js
+++ website/app.js
@@ -4,33 +4,15 @@ const baseUrl = 'api.openweathermap.org/data/2.5/weather?';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
 function convertKelvinToCelsius(kelvin) {
     return kelvin - 273.15;
 }
 
-const generate = document.getElementById('generate');
-generate.addEventListener('click', function(e) {
-    const city = document.getElementById('city').value;
-    const country = document.getElementById('country').value;
-    const feelings = document.getElementById('feelings').value;
-    console.log(`Generate button clicked. 
-    City: ${city}
-    Feeling: ${feelings}`);
 
-    weatherData = getWeather(baseUrl, key, city, country)
-        .then((weatherData) => {
-            const temperature = convertKelvinToCelsius(weatherData.main.temp);
-            postWeather('/post', { temperature, newDate, feelings })
-        })
-        .then(() => {
-            updateUI();
-        })
-})
 
-const getWeather = async(baseUrl, key, city, country) => {
-    // let fullUrl = `http://${baseUrl}q=${city},${country}&appid=${key}`;
+const getWeather = async(baseUrl, key, city) => {
     let fullUrl = `http://${baseUrl}q=${city}&appid=${key}`;
     const request = await fetch(fullUrl);
 
@@ -38,7 +20,7 @@ const getWeather = async(baseUrl, key, city, country) => {
         const data = await request.json();
         return data;
     } catch (error) {
-        console.log("error:", error);
+        console.log("getWeather error:", error);
     }
 }
 
@@ -58,30 +40,37 @@ const postWeather = async(url, data) => {
     })
 }
 
-const updateUI = async() => {
-    await console.log("updateUI");
 
+const updateUI = async() => {
     const request = await fetch('/get');
 
     try {
         const data = await request.json();
-        // console.log(data);
         let latest = data.length - 1;
 
         document.getElementById('date').innerText = data[latest]['date'];
         document.getElementById('temp').innerText = data[latest]['temperature'];
         document.getElementById('content').innerText = data[latest]['content'];
     } catch (error) {
-        console.log("error", error);
+        console.log("updateUI error", error);
     }
 }
 
-
-
-
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM loaded");
+    const generateBtn = document.getElementById('generate');
+
+    generateBtn.addEventListener('click', function(e) {
+        const city = document.getElementById('city').value;
+        const feelings = document.getElementById('feelings').value;
+
+        weatherData = getWeather(baseUrl, key, city)
+            .then((weatherData) => {
+                const temperature = Math.round(convertKelvinToCelsius(weatherData.main.temp)).toString() + "C";
+
+                postWeather('/post', { temperature, newDate, feelings })
+            })
+            .then(() => {
+                updateUI();
+            })
+    })
 })
